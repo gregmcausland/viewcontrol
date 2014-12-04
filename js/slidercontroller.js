0eb7f3acb92b0e45e277d2be8a3be516a245954d
Greg McAusland <greg.weeweb@gmail.com>
2014-12-04 13:55:34 +0000

    indentation fix

--- js/slidercontroller.js
+++ js/slidercontroller.js
@@ -1,38 +1,38 @@
 
 /* Dependancies */
-var Module 			= require('./module');
-var ViewController 	= require('./viewcontroller');
+var Module          = require('./module');
+var ViewController  = require('./viewcontroller');
 
 /* Our Module */
 var SliderController = Module.extend({
 
-	ACTIVE: 'is-active',
+    ACTIVE: 'is-active',
 
-	init: function( options ) {
-		this.super.init.call( this, options );
-		if ( this.elements.slides.length ) {
-			this.nSlides = this.elements.slides.length;
-			this.currentSlide = 0;
-			this.setSlide( 0 );
-		}
-	},
+    init: function( options ) {
+        this.super.init.call( this, options );
+        if ( this.elements.slides.length ) {
+            this.nSlides = this.elements.slides.length;
+            this.currentSlide = 0;
+            this.setSlide( 0 );
+        }
+    },
 
-	nextSlide: function() {
-		this.setSlide( this.currentSlide+1 );
-	},
+    nextSlide: function() {
+        this.setSlide( this.currentSlide+1 );
+    },
 
-	prevSlide: function() {
-		this.setSlide( this.currentSlide-1 );
-	},
+    prevSlide: function() {
+        this.setSlide( this.currentSlide-1 );
+    },
 
-	setSlide: function( targetSlide ) {
-		if ( targetSlide < 0 ) targetSlide = (this.nSlides-1);
-		if ( targetSlide > (this.nSlides-1) ) targetSlide = 0;
+    setSlide: function( targetSlide ) {
+        if ( targetSlide < 0 ) targetSlide = (this.nSlides-1);
+        if ( targetSlide > (this.nSlides-1) ) targetSlide = 0;
 
-		this.elements.slides[ this.currentSlide ].classList.remove( this.ACTIVE );
-		this.elements.slides[ targetSlide ].classList.add( this.ACTIVE );
-		this.currentSlide = targetSlide;
-	}
+        this.elements.slides[ this.currentSlide ].classList.remove( this.ACTIVE );
+        this.elements.slides[ targetSlide ].classList.add( this.ACTIVE );
+        this.currentSlide = targetSlide;
+    }
 
 });
 
