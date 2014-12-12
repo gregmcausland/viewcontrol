ebfcbc5980e35bcee9b74cd197deda63768166e7
Greg McAusland <greg.weeweb@gmail.com>
2014-12-12 12:46:14 +0000

    updates

--- js/app.js
+++ js/app.js
@@ -5,30 +5,3 @@ require('./polyfills');
 
 /* Dependencies */
 var SliderController = require('./slidercontroller');
-
-var AnimatedSliderController = SliderController.extend('moo', {
-
-  init: function( options ) {
-    SliderController.init.call(this, options);
-
-    this.options.speed = this.options.speed || 250;
-
-    this.state = {
-      animating:  ['is-animating'],
-      animateIn:  ['animate-in'],
-      active:     ['is-active'],
-      animateout: ['animate-out'],
-    }
-  },
-
-  nextSlide: function() {
-    var leavingSlide = this.currentSlide;
-    var enteringSlide = ( this.currentSlide + 1 ) % this.nSlides;
-
-    console.log( leavingSlide, enteringSlide)
-  },
-
-  prevSlide: function() {
-  }
-
-});
